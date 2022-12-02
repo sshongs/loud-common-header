ddf0df8936d417048452ef7daa0c19cd9487839e
sshong <sshong@loudcorp.com>
2022-12-02 18:18:01 +0900

    update

--- layout/Base.js
+++
@@ -1,8 +0,0 @@
-
-export default function LayoutBase({children}){
-    return(
-        <>
-              {children}
-        </>
-    )
-}
--- next-sitemap.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-    siteUrl: process.env.APP_WEBSITE_URL || 'http://localhost',
-    generateRobotsTxt: true, 
-  }
